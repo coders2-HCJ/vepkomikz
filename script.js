08f6df80c779a130af24d37cdd13060a48daca77
Avinasen <115348427+coders2-HCJ@users.noreply.github.com>
2023-07-04 18:01:16 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+var qid = new URLSearchParams(location.search).get("id");
+var titles = ["Modern Laziness"];
+document.getElementsByTagName("h6")[0].textContent = titles[parseInt(qid) - 1];
+document.getElementsByTagName("img")[0].src = qid + ".png";
