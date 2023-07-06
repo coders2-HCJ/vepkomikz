d586c57bd24797d805a8e29360a91cdd921195b4
Avinasen <115348427+coders2-HCJ@users.noreply.github.com>
2023-07-06 11:24:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var q = new URLSearchParams(location.search);
 var titles = ["Modern Laziness"];
 document.getElementsByTagName("h1")[1].textContent = titles[parseInt(q.get("id")) - 1];
 document.getElementsByTagName("img")[0].src = q.get("id") + ".png";
-document.getElementsByTagName("button")[0].onclick = function() { q.set("id", q.get("id") - 1); };
-document.getElementsByTagName("button")[1].onclick = function() { q.set("id", q.get("id") + 1); };
+document.getElementsByTagName("button")[0].onclick = function() { q.set("id", parseInt(q.get("id")) - 1); };
+document.getElementsByTagName("button")[1].onclick = function() { q.set("id", parseInt(q.get("id")) + 1); };
